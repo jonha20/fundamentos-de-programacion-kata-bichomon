ec7a3a8a8b0c22a617da5a171875b55e6c9ca040
jonha20 <64472507+jonha20@users.noreply.github.com>
2025-03-13 13:22:44 +0100

    Resueltos

--- scripts.js
+++ scripts.js
@@ -23,6 +23,7 @@ for (let i = 0; i < enlaces.length; i++) {
 
 /*7. Cambia el fondo de todos los `infocard-lg-data text-muted` para todos los Pokimon voladores `itype flying`*/
 
+//DUDA
 /*const pokemon = document.querySelectorAll(".infocard-lg-data.text-muted");
 const tipo = document.querySelectorAll(".itype.flying");
 for (let i = 0; i < pokemon.length; i++) {
@@ -32,11 +33,15 @@ for (let i = 0; i < pokemon.length; i++) {
 }*/
 
 //childNodes
+/*const array = document.querySelectorAll(".infocard-lg-data.text-muted");
+const flying = array.children
+console.log(flying)*/
 
+//Franco help
 document.querySelectorAll(".infocard-lg-data.text-muted").forEach(pokemon => {
     if (pokemon.textContent.includes("Flying")) {
         pokemon.style.background = "green";
     }
 });
 console.log(document.domain);
-console.log(window.location.href)
+console.log(window.location.href);
