af31342cba4e76a2fe347868dc7e876d0a58387d
jonha20 <64472507+jonha20@users.noreply.github.com>
2025-03-13 13:08:17 +0100

    Ejercicios Resueltos

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 /*1. Cambia el título "Generation 1 Pokémon" por "Generasión 1 Pokimon".*/
 document.getElementById("gen-1").innerHTML = "Generasión 1 Pokimon";
 /*2. Cambia el color de fondo de la primera generación de Pokimon.*/
-  document.getElementsByClassName("infocard-list infocard-list-pkmn-lg")[0].style.background =  "black";
+  document.getElementsByClassName("infocard-list infocard-list-pkmn-lg")[0].style.background = "black";
 /*3. Imprime por consola la URL de la página.*/
 console.log(window.location.href)
 /*4. Imprime por consola el dominio de la página.*/
@@ -10,7 +10,7 @@ console.log(document.domain);
 const imagenes = document.querySelectorAll("img");
 for (let i = 0; i < imagenes.length; i++) {
     console.log(imagenes[i]);
-    
+
 }
 /*6. Sustituye el atributo "src" de todas las imágenes por este "https://media.giphy.com/media/2v170e71aanfi/giphy.gif"*/
 const enlaces = document.querySelectorAll("img");
@@ -23,24 +23,20 @@ for (let i = 0; i < enlaces.length; i++) {
 
 /*7. Cambia el fondo de todos los `infocard-lg-data text-muted` para todos los Pokimon voladores `itype flying`*/
 
-/*const pokemon = document.querySelectorAll(".infocard-lg-data text-muted");
-const tipo = document.querySelectorAll(".itype flying");
-console.log(pokemon[0])
+/*const pokemon = document.querySelectorAll(".infocard-lg-data.text-muted");
+const tipo = document.querySelectorAll(".itype.flying");
 for (let i = 0; i < pokemon.length; i++) {
-    for (let x = 0; x < tipo.length; x++) {
-        if (tipo.innerHTML == "Flying") {
+        if (tipo[i].textContent.includes("Flying")) {
             pokemon[i].style.background = "green";
         }
-    }
-    
 }*/
-document.querySelectorAll(".infocard-lg-data text-muted").forEach(function (pokemon) {
-    document.querySelectorAll("itype flying").forEach(function (tipo) {
-        if (tipo.className == "itype flying") {
-            pokemon[i].style.background = "green";
-        }
-    })
-})
 
+//childNodes
+
+document.querySelectorAll(".infocard-lg-data.text-muted").forEach(pokemon => {
+    if (pokemon.textContent.includes("Flying")) {
+        pokemon.style.background = "green";
+    }
+});
 console.log(document.domain);
 console.log(window.location.href)
